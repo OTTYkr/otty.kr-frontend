90e46e91bc32f48c9dc4f3611488106fe3901941
kbhetrr <kimkh7534@naver.com>
2023-12-05 16:53:27 +0900

    KKH: 메인 페이지 시간 동기화

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -28,13 +28,13 @@ const Home = async () => {
       <div className="px-2 sm:px-4 md:px-6 lg:px-8 text-xs sm:text-sm text-right w-full max-w-4xl justify-end">
         {nowDate.getFullYear() +
           "." +
-          String(nowDate.getMonth() + 1).padStart(2, "0") +
+          String(nowDate.getUTCMonth() + 1).padStart(2, "0") +
           "." +
-          String(nowDate.getDate()).padStart(2, "0") +
+          String(nowDate.getUTCDate()).padStart(2, "0") +
           " " +
-          String(nowDate.getHours()).padStart(2, "0") +
+          String(nowDate.getUTCHours()).padStart(2, "0") +
           ":" +
-          String(nowDate.getMinutes()).padStart(2, "0") +
+          String(nowDate.getUTCMinutes()).padStart(2, "0") +
           " "}{" "}
         기준
       </div>
