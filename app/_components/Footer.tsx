490cde3250c4a46bc6c9280464b2e710a9c0e44d
kbhetrr <kimkh7534@naver.com>
2023-12-05 16:41:13 +0900

    KKH: 푸터 수정

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="relative mx-auto flex h-fit min-w-[360px] flex-col items-center justify-center gap-y-5 bg-zinc-50 py-12 text-zinc-500">
       <section className="w-full max-w-[1700px] p-5 pb-0 text-xs text-center flex flex-col items-center">
         <h3 className="mb-2 text-base font-bold">otty.kr</h3>
-        <p>오티 | 주소: 대한민국</p>
+        <p>오티 (OTTY) | 주소: 대한민국</p>
       </section>
     </footer>
   );
