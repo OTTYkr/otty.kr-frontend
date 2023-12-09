4a4b2e34cb4166d6ab7d509102028b1491e5163f
kbhetrr <kimkh7534@naver.com>
2023-12-09 21:30:01 +0900

    KKH: 네비게이션 바 애니메이션 추가

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,8 +2,10 @@
 import Image from "next/image";
 import logo from "../../public/logo.png";
 import navmenu from "../../public/navmenu.svg";
+import cancel from "../../public/cancel.svg";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+
 const Header = () => {
   const [isOpen, setisOpen] = useState(false);
   // 스크롤이 50px 이상 내려올경우 true값을 넣어줄 useState
@@ -14,7 +16,8 @@ const Header = () => {
     return () => {
       window.removeEventListener("scroll", handleScroll); //clean up
     };
-  }, []);
+  });
+
   const handleScroll = () => {
     // 스크롤이 Top에서 50px 이상 내려오면 true값을 useState에 넣어줌
     if (window.scrollY >= 10) {
@@ -25,22 +28,23 @@ const Header = () => {
       setScroll(false);
     }
   };
+
   return (
     <nav
       className={
         scroll
-          ? "fixed top-0 left-0 right-0 z-50 mx-auto h-16 w-full min-w-[360px] border-b shadow-md transition-shadow bg-white text-lg font-semibold text-zinc-800"
-          : "fixed top-0 left-0 right-0 z-50 mx-auto h-16 w-full min-w-[360px] bg-white text-lg transition-shadow font-semibold text-zinc-800"
+          ? "fixed top-0 left-0 right-0 z-50 mx-auto max-h-fit w-full min-w-[360px] border-b shadow-md bg-white text-lg font-semibold text-zinc-800"
+          : "fixed top-0 left-0 right-0 z-50 mx-auto max-h-fit w-full min-w-[360px] bg-white text-lg font-semibold text-zinc-800"
       }
     >
-      <div className="mx-auto flex h-full max-w-[1000px] items-center justify-between gap-5 xs:gap-2">
+      <div className="mx-auto flex h-full max-w-[1000px] items-center justify-between gap-5 xs:gap-2 py-2.5">
         <Link href="/" className="flex px-4">
           <Image className="w-8 h-8" src={logo} alt="로고" priority />
           <div className="flex items-center justify-center pl-0.5 text-base sm:text-lg">
             otty
           </div>
         </Link>
-        <div className="flex flex-row">
+        <div className="flex-row hidden lg:flex">
           <Link
             href="/global"
             className="flex items-center justify-center px-4 py-2 text-base transition-all rounded-md whitespace-nowrap hover:bg-zinc-200 2xs:px-2 sm:text-lg"
@@ -53,8 +57,39 @@ const Header = () => {
           >
             인기 뉴스
           </Link>
-          <Image className="w-8 h-8 lg:hidden" src={navmenu} alt="menu" />
         </div>
+        <Image
+          className="w-12 h-12 pr-4 lg:hidden"
+          src={isOpen ? cancel : navmenu}
+          alt="menu"
+          onClick={() => setisOpen(!isOpen)}
+        />
+      </div>
+      {/* mobile menu */}
+      <div
+        className={
+          "lg:hidden transition-all duration-200 ease-in-out " +
+          (isOpen ? "h-[120px]" : "h-0")
+        }
+      >
+        {isOpen ? (
+          <div className="w-full">
+            <Link
+              href="#"
+              className="block px-10 py-5 text-sm hover:bg-gray-200"
+            >
+              시가총액 순위
+            </Link>
+            <Link
+              href="#"
+              className="block px-10 py-5 text-sm hover:bg-gray-200"
+            >
+              인기 뉴스
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </nav>
   );
