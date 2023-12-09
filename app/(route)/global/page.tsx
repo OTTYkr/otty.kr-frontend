3427c786b0f800489811da114520992ea65fed75
kbhetrr <kimkh7534@naver.com>
2023-12-09 20:06:58 +0900

    KKH: 페이지 구성 수정 중

---
+++ app/(route)/global/page.tsx
@@ -0,0 +1,49 @@
+import StockRankList from "../../_components/StockRankList";
+
+async function getData() {
+  try {
+    const res = await fetch(process.env.API_URL + "/api/stock_rank", {
+      next: { revalidate: 3600 },
+    });
+
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    return { data: null };
+  }
+}
+
+const Global = async () => {
+  let RankData = await getData();
+  const nowDate = new Date(RankData[0]["date"]);
+  return (
+    <div className="w-full max-w-4xl">
+      <div className="p-4 text-xl font-extrabold text-center">
+        실시간 세계 시가총액 순위
+      </div>
+      <div className="justify-end w-full max-w-4xl px-2 text-xs text-right sm:px-4 md:px-6 lg:px-8 sm:text-sm">
+        {nowDate.getFullYear() +
+          "." +
+          String(nowDate.getMonth() + 1).padStart(2, "0") +
+          "." +
+          String(nowDate.getDate()).padStart(2, "0") +
+          " " +
+          String(nowDate.getHours()).padStart(2, "0") +
+          ":" +
+          String(nowDate.getMinutes()).padStart(2, "0") +
+          " "}{" "}
+        기준
+      </div>
+      <div className="flex flex-row w-full p-2 lg:p-3 bg-slate-200">
+        <div className="w-1/12 text-center">랭킹</div>
+        <div className="flex-1 text-left ">기업명</div>
+        <div className="w-1/3 text-right">시가총액</div>
+        <div className="w-1/6 text-center">변동</div>
+        <div className="w-1/12 text-center">국가</div>
+      </div>
+      <StockRankList data={RankData} />
+    </div>
+  );
+};
+
+export default Global;
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,22 +1,35 @@
+"use client";
 import Image from "next/image";
 import logo from "../../public/logo.png";
+import navmenu from "../../public/navmenu.svg";
 import Link from "next/link";
+import { useState } from "react";
 const Header = () => {
+  const [isOpen, setisOpen] = useState(false);
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 mx-auto h-16 w-full min-w-[360px] border-b bg-white px-4 md:px-16 py-4 text-lg font-semibold text-zinc-800">
+    <nav className="fixed top-0 left-0 right-0 z-50 mx-auto h-16 w-full min-w-[360px] border-b bg-white text-lg font-semibold text-zinc-800">
       <div className="mx-auto flex h-full max-w-[1000px] items-center justify-between gap-5 xs:gap-2">
-        <Link href="/" className="flex">
+        <Link href="/" className="flex px-4">
           <Image className="w-8 h-8" src={logo} alt="로고" priority />
           <div className="flex items-center justify-center pl-0.5 text-base sm:text-lg">
             otty
           </div>
         </Link>
-        <Link
-          href="/news"
-          className="flex items-center justify-center px-4 py-2 text-base transition-all rounded-md whitespace-nowrap hover:bg-zinc-200 2xs:px-2 sm:text-lg"
-        >
-          인기 뉴스
-        </Link>
+        <div className="flex flex-row">
+          <Link
+            href="/global"
+            className="flex items-center justify-center px-4 py-2 text-base transition-all rounded-md whitespace-nowrap hover:bg-zinc-200 2xs:px-2 sm:text-lg"
+          >
+            시가총액 순위
+          </Link>
+          <Link
+            href="/news"
+            className="flex items-center justify-center px-4 py-2 text-base transition-all rounded-md whitespace-nowrap hover:bg-zinc-200 2xs:px-2 sm:text-lg"
+          >
+            인기 뉴스
+          </Link>
+          <Image className="w-8 h-8 lg:hidden" src={navmenu} alt="menu" />
+        </div>
       </div>
     </nav>
   );
