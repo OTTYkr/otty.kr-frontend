41e4db6b04ec8ab351187f82518c0ddc1ea172d1
kbhetrr <kimkh7534@naver.com>
2023-12-10 22:19:37 +0900

    KKH: sitemap, robots.txt 설정

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://otty.kr",
+  changefreq: "daily",
+  priority: 1,
+  sitemapSize: 7000,
+  generateRobotsTxt: true,
+  exclude: [],
+};
