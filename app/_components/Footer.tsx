b44b369b6bc0b97d71e3354ecde87eebd107ed51
kbhetrr <kimkh7534@naver.com>
2023-12-15 23:11:28 +0900

    KKH: 푸터 로고 추가

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,6 +3,7 @@ import Instagram from "../../public/instagram.svg";
 import Facebook from "../../public/facebook.svg";
 import Email from "../../public/email.svg";
 import Image from "next/image";
+import BrandLogo from "../../public/logo.svg";
 
 const Footer = () => {
   return (
@@ -35,34 +36,43 @@ const Footer = () => {
             <li>이메일</li>
           </ul>
         </div>
-        <div className="px-4 lg:px-0">
-          <h3 className="mb-2 text-base font-bold">오티 | OTTY</h3>
-          <p className="leading-[25px]">
-            주식 가격 및 시가총액은 서버 상태에 따라 지연될 수 있습니다. <br />{" "}
-            ⓒ {new Date().getFullYear()}. OTTY. all rights reserved.
-          </p>
-          <div className="flex flex-row pt-4">
-            <Link
-              className="p-2 rounded-full bg-slate-200"
-              href="https://www.instagram.com/otty.kr"
-              target="_blank"
-            >
-              <Image
-                src={Instagram}
-                className="w-[20px] h-[20px]"
-                alt="instagram"
-              />
-            </Link>
-            <div className="p-2 ml-2 rounded-full bg-slate-200">
-              <Image
-                src={Facebook}
-                className="w-[20px] h-[20px]"
-                alt="facebook"
-              />
+        <div className="flex flex-col-reverse justify-between px-4 md:flex-row lg:px-0">
+          <div>
+            <h3 className="mb-2 text-base font-bold">오티 | OTTY</h3>
+            <p className="leading-[25px]">
+              주식 가격 및 시가총액은 서버 상태에 따라 지연될 수 있습니다.{" "}
+              <br /> ⓒ {new Date().getFullYear()}. OTTY. all rights reserved.
+            </p>
+            <div className="flex flex-row pt-4">
+              <Link
+                className="p-2 rounded-full bg-slate-200"
+                href="https://www.instagram.com/otty.kr"
+                target="_blank"
+              >
+                <Image
+                  src={Instagram}
+                  className="w-[20px] h-[20px]"
+                  alt="instagram"
+                />
+              </Link>
+              <div className="p-2 ml-2 rounded-full bg-slate-200">
+                <Image
+                  src={Facebook}
+                  className="w-[20px] h-[20px]"
+                  alt="facebook"
+                />
+              </div>
+              <div className="p-2 ml-2 rounded-full bg-slate-200">
+                <Image src={Email} className="w-[20px] h-[20px]" alt="email" />
+              </div>
             </div>
-            <div className="p-2 ml-2 rounded-full bg-slate-200">
-              <Image src={Email} className="w-[20px] h-[20px]" alt="email" />
-            </div>
+          </div>
+          <div className="object-contain mb-4 w-fit h-fit md:mb-0">
+            <Image
+              className="w-12 h-12 md:w-[64px] md:h-[64px]"
+              src={BrandLogo}
+              alt="OTTY"
+            />
           </div>
         </div>
       </section>
