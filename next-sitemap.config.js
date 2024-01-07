88e07afcd415f2118b9d8ac918d4ca90d6566f7c
kbhetrr <kimkh7534@naver.com>
2024-01-07 12:48:55 +0900

    KKH: next-sitemap.config.js 삭제

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://otty.kr",
-  changefreq: "daily",
-  priority: 1,
-  sitemapSize: 7000,
-  generateRobotsTxt: true,
-  exclude: [],
-};
